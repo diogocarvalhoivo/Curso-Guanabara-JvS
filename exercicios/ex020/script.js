0753e7d8faf49b94df012bce89a5baa5395b53b7
Diogo Carvalho Ivo <diogocivo@gmail.com>
2023-09-13 13:18:47 +0100

    cs

--- exercicios/ex020/script.js
+++ exercicios/ex020/script.js
@@ -1,27 +1,49 @@
 let vetor =[]
+let res = document.querySelector('#res')
 
 function adicionar(){
     let num = Number(document.querySelector('#num').value)
-    let res = document.querySelector('#res')
-    
+    let i=0
     if (num>100 || num<1){
         
     } else{
-        for (let c=0; c<vetor.length+1; c++){
+        for (let c=0; c<vetor.length; c++){
             if (vetor[c]==num){
-
-            } else{
-                vetor.push(num)
-
-                let tab = document.querySelector('#selquadro')
-                let item = document.createElement('option') /*cria um option*/
-                item.text = `Valor ${num} adicionado` /*informa o q será escrito no option*/
-                item.value = `${num}` /*valores a serem gravados para enviar para um banco de dados*/
-                tab.appendChild(item) /*cria o option dentro de tab, que é referente ao elemento select do html */
-        
-                
-                res.innerHTML=`${vetor}`
+                i = 1
+            }
+        }
+        if (i==1){
+            window.alert(`Número já usado`)
+        } else{
+            vetor.push(num)
+            let tab = document.querySelector('#selquadro')
+            let item = document.createElement('option') /*cria um option*/
+            item.text = `Valor ${num} adicionado` /*informa o q será escrito no option*/
+            item.value = `${num}` /*valores a serem gravados para enviar para um banco de dados*/
+            tab.appendChild(item) /*cria o option dentro de tab, que é referente ao elemento select do html */
+            res.innerHTML= ``
+            if (res.innerHTML != ``){
+                res.innerHTML= ``
             }
         }  
+    }      
+}
+
+function finalizar(){
+    function sortfunction(a,b){
+        return a-b
     }
+    vetor.sort(sortfunction)
+    let maior = vetor[vetor.length-1]
+    let menor = vetor[0]
+    let soma=0
+    for (let c=0; c<vetor.length; c++){
+        soma+=vetor[c]
+    }
+    let media = soma/vetor.length
+    res.innerHTML =`Ao todo, temos ${vetor.length} números cadastrados.<br>
+    O maior valor informado foi ${maior}<br>
+    O menor valor informado foi ${menor}<br>
+    Somando todos o valores, temos ${soma}<br>
+    A média dos valores digitados é ${media}.`
 }
--- teste.js
+++ teste.js
@@ -1,16 +1,31 @@
-let vetor =[]
-let num = 5
+let i=0
+let num = 3
+let vetor=[1,12]
 
 if (num>100 || num<1){
     
 } else{
-    for (let c=0; c<vetor.length+1; c++){
+    for (let c=0; c<vetor.length; c++){
         if (vetor[c]==num){
-
-        } else{
-            vetor.push(num)
-
-            console.log(`${vetor}`)
+            i = 1
         }
+    }
+    if (i==1){
+
+    } else{
+        vetor.push(num)
     }  
 }
+
+function sortfunction(a,b){
+    return (a-b)
+}
+
+vetor.sort(sortfunction)
+let maior = vetor[vetor.length-1]
+let menor = vetor[0]
+let soma=0
+for (let c=0; c<vetor.length; c++){
+    soma+=vetor[c]
+}
+let media = soma/vetor.length
